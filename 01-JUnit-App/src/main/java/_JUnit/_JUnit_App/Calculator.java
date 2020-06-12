69da6976b1186664eef325d1c015352dccfcbfd5
Java-Project-01-11-30AMexit <okothapally74@gmail.com>
2020-06-12 22:49:45 +0530

    Branch practice master

---
+++ 01-JUnit-App/src/main/java/_JUnit/_JUnit_App/Calculator.java
@@ -0,0 +1,9 @@
+package _JUnit._JUnit_App;
+
+public class Calculator {
+	
+	public Integer add(Integer a, Integer b) {
+		return a + b;
+	}
+
+}
---
+++ 01-JUnit-App/src/test/java/_JUnit/_JUnit_App/CalculatorTest.java
@@ -0,0 +1,19 @@
+package _JUnit._JUnit_App;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTest 
+   
+{
+	@Test
+    public void testAdd_01() {
+    	Calculator c= new Calculator();
+    	Integer actualResult = c.add(10, 20);
+    	Integer expectedResult=30;
+    	
+    	assertEquals(expectedResult, actualResult);
+    }
+    
+}
